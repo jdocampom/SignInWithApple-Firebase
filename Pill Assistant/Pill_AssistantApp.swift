b2a072633ae02c653c94dec5a242054a3dde94a0
jdocampom <gummy_pond0v@icloud.com>
2022-03-15 14:43:52 -0500

    Initial Commit

---
+++ Pill Assistant/Pill_AssistantApp.swift
@@ -0,0 +1,20 @@
+//
+//  Pill_AssistantApp.swift
+//  Pill Assistant
+//
+//  Created by Juan Diego Ocampo on 15/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Pill_AssistantApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
